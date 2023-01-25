5ddcf8e7589c131f6f2a08306d3dbc8436c0a626
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-01-25 01:35:49 -0500

    operators in c++

---
+++ operators/Operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,y;
+    a=5; //assignment operator
+    y=a++;
+    cout<<"Value of a: "<<y<<"\n";
+    //a++;
+
+    cout<<"Value of a: "<<a<<"\n";
+    return 0;
+}
