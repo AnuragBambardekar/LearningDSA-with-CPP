d72b13053e13a551a7c614f40880178eb5f2d46c
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-01-28 01:26:35 -0500

    even or odd numbers

---
+++ evenOdd/evenOdd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter an integer number: "<<endl;
+    cin>>number;
+
+    if(number%2 == 0)
+    {
+        cout<<number<<" is an even number.";
+    }
+    else {
+        cout<<number<<" is an odd number.";
+    }
+
+
+    return 0;
+}
