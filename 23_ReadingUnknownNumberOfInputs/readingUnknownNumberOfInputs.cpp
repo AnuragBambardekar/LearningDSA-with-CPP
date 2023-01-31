13013eca0c7732eae7e4c09975e405c7e0227f90
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-01-31 13:18:41 -0500

    istream as a test condition

---
+++ 23_ReadingUnknownNumberOfInputs/readingUnknownNumberOfInputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int value = 0, sum = 0;
+    while(cin>>value) //Ctrl+Z to exit
+    {
+        sum =+ value;
+    }
+    cout<<sum;
+    return 0;
+}
