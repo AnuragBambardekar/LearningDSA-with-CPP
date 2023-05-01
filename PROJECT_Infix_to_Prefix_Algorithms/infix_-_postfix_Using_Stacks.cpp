2c12b0ca33f03eadec5ef97a82eb7c9405d4025b
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-05-01 12:14:57 -0400

    unbalanced parantheses check added

--- PROJECT_Infix_to_Prefix_Algorithms/infix_-_postfix_Using_Stacks.cpp
+++ PROJECT_Infix_to_Prefix_Algorithms/infix_-_postfix_Using_Stacks.cpp
@@ -16,9 +16,48 @@ int precedence(char op) {
     return 0;
 }
 
+// string infixToPostfix(string expression) {
+//     string result = "";
+//     stack<char> operators;
+
+//     for (int i = 0; i < expression.length(); i++) {
+//         char current = expression[i];
+
+//         if (isalnum(current)) {
+//             result += current;
+//         } else if (current == '(') {
+//             operators.push(current);
+//         } else if (current == ')') {
+//             while (!operators.empty() && operators.top() != '(') {
+//                 result += operators.top();
+//                 operators.pop();
+//             }
+//             if (!operators.empty() && operators.top() == '(') {
+//                 operators.pop();
+//             }
+//         } else {
+//             while (!operators.empty() && precedence(current) <= precedence(operators.top())) {
+//                 result += operators.top();
+//                 operators.pop();
+//             }
+//             operators.push(current);
+//         }
+//     }
+
+//     while (!operators.empty()) {
+//         result += operators.top();
+//         operators.pop();
+//     }
+
+//     return result;
+// }
+
+//check for unbalanced parantheses
 string infixToPostfix(string expression) {
     string result = "";
     stack<char> operators;
+    int numOpeningParentheses = 0;
+    int numClosingParentheses = 0;
 
     for (int i = 0; i < expression.length(); i++) {
         char current = expression[i];
@@ -27,7 +66,9 @@ string infixToPostfix(string expression) {
             result += current;
         } else if (current == '(') {
             operators.push(current);
+            numOpeningParentheses++;
         } else if (current == ')') {
+            numClosingParentheses++;
             while (!operators.empty() && operators.top() != '(') {
                 result += operators.top();
                 operators.pop();
@@ -49,8 +90,13 @@ string infixToPostfix(string expression) {
         operators.pop();
     }
 
+    if (numOpeningParentheses != numClosingParentheses) {
+        throw invalid_argument("Unbalanced parentheses");
+    }
+
     return result;
 }
+
 
 int evaluatePostfix(string expression) {
     stack<int> operands;
@@ -92,15 +138,21 @@ int evaluatePostfix(string expression) {
 int main() {
     vector<string> expressions = {"3+4*2/(1-5)^2", "5*(7-2)/3+8",
                                   "9*8/4-2+6/3+1", "(4+5)*2-8/4+9",
-                                  "6*7-4/2+3*(8-5)","(2+3*4)10&6|4^2"};
+                                  "6*7-4/2+3*(8-5)","(2+3*4)10&6|4^2",
+                                  "((6+4)-2"};
     for (auto& expression : expressions) {
-        cout << "Infix Expression: " << expression << endl;
+    cout << "Infix Expression: " << expression << endl;
+    try {
         string postfixExpression = infixToPostfix(expression);
         cout << "Postfix Expression: " << postfixExpression << endl;
         int result = evaluatePostfix(postfixExpression);
         cout << "Result: " << result << endl;
-        cout << endl;
+    } catch (const invalid_argument& e) {
+        cout << "Error: " << e.what() << endl;
     }
+    cout << endl;
+}
+
     return 0;
 }
 
